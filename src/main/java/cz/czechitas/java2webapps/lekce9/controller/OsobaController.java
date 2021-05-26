6c56ed36b4a3489b2d97e0f1006c747d483ef07d
Filip Jirsák <filip@jirsak.org>
2021-05-26 21:47:32 +0200

    Předávat do formuláře aktuální URL s parametry a bez size a page.

--- src/main/java/cz/czechitas/java2webapps/lekce9/controller/OsobaController.java
+++ src/main/java/cz/czechitas/java2webapps/lekce9/controller/OsobaController.java
@@ -5,7 +5,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UrlPathHelper;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Controller pro zobrazování seznamů osob.
@@ -27,5 +33,23 @@ public class OsobaController {
   @GetMapping("/vyber")
   public String vyber() {
     return "vyber";
+  }
+
+  /**
+   * Získání aktuální URL s query parametry bez parametrů {@code size} a {@code page}.
+   *
+   * Je to ošklivé, ale dělá to, co potřebuju…
+   */
+  @ModelAttribute("currentURL")
+  public String currentURL(HttpServletRequest request) {
+    UrlPathHelper helper = new UrlPathHelper();
+    return UriComponentsBuilder
+            .newInstance()
+            .path(helper.getOriginatingRequestUri(request))
+            .query(helper.getOriginatingQueryString(request))
+            .replaceQueryParam("size")
+            .replaceQueryParam("page")
+            .build()
+            .toString();
   }
 }
